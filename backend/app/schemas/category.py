40fa1fa57790f389ce1ed014f749d89a38cd2a4c
Даниил <danikzhurik64@gmail.com>
2026-04-25 15:47:34 +0300

    add small fixes after test

--- backend/app/schemas/category.py
+++ backend/app/schemas/category.py
@@ -1,18 +1,21 @@
 from pydantic import BaseModel, Field
 
-class CategoryBase(BaseModel): # ОБЩИЕ поля для остальных моделей
-    name: str = Field(..., min_length=5, max_length=100,
-        description="Category name")
-    slug: str = Field(..., min_length=5, max_length=100,
-        description="URL-friendly category name")
-    
+
+class CategoryBase(BaseModel):  # ОБЩИЕ поля для остальных моделей
+    name: str = Field(..., min_length=5, max_length=100, description="Category name")
+    slug: str = Field(
+        ..., min_length=5, max_length=100, description="URL-friendly category name"
+    )
+
+
 class CategoryCreate(CategoryBase):
     pass
-    
-class CategoryResponse(CategoryBase): # для просмотра детальной информации о категории
-    id: int = Field(..., description='Unique category identifier') # ... значит, что обязательное поле
-    
+
+
+class CategoryResponse(CategoryBase):  # для просмотра детальной информации о категории
+    id: int = Field(
+        ..., description="Unique category identifier"
+    )  # ... значит, что обязательное поле
+
     class Config:
-        form_attributes = True # позволяет создавать схему напрямую из модели 
-        
-        
+        from_attributes = True  # позволяет создавать схему напрямую из модели
